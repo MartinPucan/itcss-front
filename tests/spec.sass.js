9748148fb3f80227f3265b2ff3d0e9eb81a05772
MartinPucan <pucan.m@seznam.cz>
2021-03-11 13:02:10 +0100

    create itcss structure

--- tests/spec.sass.js
+++
@@ -1,5 +0,0 @@
-var path = require('path');
-var sassTrue = require('sass-true');
-
-var sassFile = path.join(__dirname, 'true-sass-tests/sass_tests.scss');
-sassTrue.runSass({file: sassFile}, {describe, it});
